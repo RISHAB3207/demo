519b4b90d17d2c79d46ee91b5cc67abdd151af96
Rishab Chaudhary <rishab3207@gmail.com>
2022-06-06 19:29:05 +0530

    changes

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,7 @@
 const axios=require("axios");
 
 exports.homeRoutes=(req , res)=>{
-    axios.get('https://rocky-brushlands-79029.herokuapp.com/api/users')
+    axios.get('http://localhost:3000zz/api/users')
     .then(function(response){
         res.render('index',{users : response.data});
 
@@ -18,7 +18,7 @@ exports.add_user=(req,res)=>{
 
 
 exports.update_user = (req, res) =>{
-    axios.get('https://rocky-brushlands-79029.herokuapp.com/api/user', { params : { id : req.query.id }})
+    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
